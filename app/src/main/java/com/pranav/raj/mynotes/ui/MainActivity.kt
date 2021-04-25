f6a0a892e3b7ca84a6d54fdb11ce61423ff5f612
PranavRaj12 <rajpranav123@gmail.com>
2021-04-25 11:24:26 +0530

    Navigation components

--- app/src/main/java/com/pranav/raj/mynotes/ui/MainActivity.kt
+++ app/src/main/java/com/pranav/raj/mynotes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pranav.raj.mynotes
+package com.pranav.raj.mynotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pranav.raj.mynotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/pranav/raj/mynotes/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.pranav.raj.mynotes.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.pranav.raj.mynotes.R
+
+class TasksFragment : Fragment (R.layout.fragment_tasks) {
+}
